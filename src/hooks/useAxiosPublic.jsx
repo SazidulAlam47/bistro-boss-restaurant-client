3f376b035197cf2a3fdb9726de0158ed53200cc9
Sazidul Alam <sazidulalam20@gmail.com>
2024-05-23 20:44:37 +0600

    deploy

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
+export const baseUrl =
+    "https://bistro-boss-restaurant-server-delta-teal.vercel.app/";
 
 const axiosPublic = axios.create({
     baseURL: baseUrl,
