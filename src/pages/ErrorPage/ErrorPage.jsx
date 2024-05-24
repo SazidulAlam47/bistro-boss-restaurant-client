67fa7e580aea9fce596138eb822360bb31e9b9b1
Sazidul Alam <sazidulalam20@gmail.com>
2024-05-24 22:59:20 +0600

    helmet fix

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     return (
         <div className="flex flex-col gap-3 justify-center items-center h-screen">
             <Helmet>
-                <title>Travel Guru | 404</title>
+                <title>Bistro Boss | 404</title>
             </Helmet>
             <img src="/images/404.gif" alt="error" className="w-[30%]" />
             <h3 className="font-bold text-2xl">404 - PAGE NOT FOUND</h3>
